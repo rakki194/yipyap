719fcaeef6e6684d45e709ca59911df70d8e5709
Balazs Horvath <acsipont@gmail.com>
2024-11-24 11:05:21 +0100

    native keydown handler, skip action if input elements are focused

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,19 +1,58 @@
 // src/components/Gallery/Gallery.tsx
-import { createSignal, createEffect, Show } from "solid-js";
+import { Show, onMount, onCleanup } from "solid-js";
 import { Controls } from "./Controls";
 import { ImageGrid } from "./ImageGrid";
 import { ImageModal } from "../ImageViewer/ImageModal";
 import { useGallery } from "~/contexts/GalleryContext";
-import type { ImageItem } from "~/resources/browse";
-import { createShortcut } from "@solid-primitives/keyboard";
 
 export const Gallery = () => {
   const gallery = useGallery();
   let gridRef: HTMLDivElement | undefined;
 
-  createShortcut(["ArrowRight"], gallery.selectNext);
-  createShortcut(["ArrowLeft"], gallery.selectPrev);
-  createShortcut(["Enter"], gallery.toggleEdit);
+  const keyDownHandler = (event: KeyboardEvent) => {
+    if (!event) return;
+
+    // Don't act if a input element is focused
+    const activeElement = document.activeElement as HTMLElement;
+    if (
+      activeElement &&
+      (activeElement.isContentEditable ||
+        ["INPUT", "TEXTAREA"].includes(activeElement.tagName))
+    ) {
+      return;
+    }
+
+    // console.log(
+    //   "keyDownEvent",
+    //   event,
+    //   activeElement &&
+    //     (activeElement.isContentEditable ||
+    //       ["INPUT", "TEXTAREA"].includes(activeElement.tagName))
+    // );
+
+    // Don't act if a modifier key is pressed
+    if (event.shiftKey || event.altKey || event.ctrlKey) {
+      return;
+    }
+
+    if (event.key === "ArrowRight") {
+      gallery.selectNext();
+    } else if (event.key === "ArrowLeft") {
+      gallery.selectPrev();
+    } else if (event.key === "Enter") {
+      gallery.toggleEdit();
+    } else if (event.key === "Escape") {
+      gallery.setMode("view");
+    } else {
+      return;
+    }
+    event.preventDefault();
+  };
+
+  onMount(() => {
+    window.addEventListener("keydown", keyDownHandler);
+    onCleanup(() => window.removeEventListener("keydown", keyDownHandler));
+  });
 
   return (
     <>
@@ -37,7 +76,7 @@ export const Gallery = () => {
                 gridRef={(el) => (gridRef = el)}
               />
             </div>
-            <Show when={gallery.editedImage && gallery.editedImage}>
+            <Show when={gallery.editedImage}>
               {(image) => (
                 <ImageModal
                   image={image()}
