12b0fc11f42aa692bafc075ef70597c8b48d9004
Balazs Horvath <acsipont@gmail.com>
2024-12-05 16:25:04 +0100

    this is a bubble update

--- src/components/ImageViewer/TagBubble.tsx
+++ src/components/ImageViewer/TagBubble.tsx
@@ -1,22 +1,36 @@
 import { Component } from "solid-js";
-import { DeleteIcon } from "~/icons";
+import { DismissIcon } from "~/icons";
 
 export const TagBubble: Component<{
   tag: string;
   onRemove: () => void;
 }> = (props) => {
+  const getTagColor = (tag: string) => {
+    // Generate a consistent hue based on the tag string
+    let hash = 0;
+    for (let i = 0; i < tag.length; i++) {
+      hash = tag.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    const hue = hash % 360;
+    // Use a muted saturation and higher lightness for a lighter look
+    return `hsl(${hue}, 40%, 85%)`;
+  };
+
   return (
-    <span class="tag-bubble">
+    <span
+      class="tag-bubble"
+      style={{
+        "background-color": getTagColor(props.tag),
+        color: "var(--text-secondary)", // Darker text for better contrast on light backgrounds
+      }}
+    >
       <span class="tag-text">{props.tag}</span>
       <button
         type="button"
         class="icon remove-tag"
-        style={{
-          border: "none",
-        }}
         onClick={props.onRemove}
         title="Remove tag"
-        innerHTML={DeleteIcon}
+        innerHTML={DismissIcon}
       />
     </span>
   );
