decdb8328c483cdff2d468e96a4b91bd7bb87bf2
Balazs Horvath <acsipont@gmail.com>
2024-12-26 02:50:05 +0100

    Enhance Toggle Component with Focus Styles and Animation Control
    - Added a new focus style for the toggle knob when checked and focused, improving accessibility and visual feedback.
    - Updated transition properties for smoother animations across the toggle component, utilizing CSS variables for consistency.
    - Introduced a 'no-animation' class to disable animations, allowing users to customize their experience based on preferences.
    
    These changes aim to enhance the user experience by providing better visual cues and flexibility in animation settings for the toggle component.

--- src/components/Toggle/Toggle.tsx
+++ src/components/Toggle/Toggle.tsx
@@ -1,4 +1,5 @@
 import { Component } from "solid-js";
+import { useAppContext } from "~/contexts/app";
 import "./Toggle.css";
 
 interface ToggleProps {
@@ -9,10 +10,13 @@ interface ToggleProps {
 }
 
 export const Toggle: Component<ToggleProps> = (props) => {
+  const app = useAppContext();
+
   return (
     <input
       type="checkbox"
       class="toggle"
+      classList={{ "no-animation": app.disableAnimations }}
       checked={props.checked}
       onChange={(e) => props.onChange(e.currentTarget.checked)}
       title={props.title}
